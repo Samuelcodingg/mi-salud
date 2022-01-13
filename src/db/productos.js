c67c7af9a4628f2ece9d5a789903a0a95b15ce66
Samuelcodingg <samuelroman39@gmail.com>
2022-01-13 15:28:15 -0500

    data agregada para los productos

--- src/db/productos.js
+++ src/db/productos.js
@@ -5,6 +5,223 @@ export const productos = [
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
         "price": "S/. 10.00",
         "bigImg": "pampersBig",
-        "smallImg": "pampersSmall"
+        "smallImg": "pampersSmall",
+        "tipo": "infantil"
+    },
+    {
+        "id": "2",
+        "name": "Crema de Afeitar Gillete",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 14.00",
+        "bigImg": "gilleteBig",
+        "smallImg": "gilleteSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "3",
+        "name": "Máquina de Afeitar Gillete",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 9.00",
+        "bigImg": "maqGilleteBig",
+        "smallImg": "maqGilleteSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "4",
+        "name": "Shampoo H&S",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "shampooBig",
+        "smallImg": "shampooSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "5",
+        "name": "Jabón Dove Clásico",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 6.00",
+        "bigImg": "jabonBig",
+        "smallImg": "jabonSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "6",
+        "name": "Paquete de 4 cepillos Colgate",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 15.00",
+        "bigImg": "cepilloBig",
+        "smallImg": "cepilloSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "7",
+        "name": "Peine de Ducha",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 10.00",
+        "bigImg": "peineBig",
+        "smallImg": "peineSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "8",
+        "name": "Hilo Dental Oral B",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 10.00",
+        "bigImg": "hiloBig",
+        "smallImg": "hiloSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "9",
+        "name": "Enjuague Bucal Colgate",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 11.00",
+        "bigImg": "enjuagueBig",
+        "smallImg": "enjuagueSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "10",
+        "name": "The Bear Beria Gel",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 14.00",
+        "bigImg": "bearBig",
+        "smallImg": "bearSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "11",
+        "name": "Acondicionador Pantene Clásico",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "acondicionadorBig",
+        "smallImg": "acondicionadorSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "12",
+        "name": "Jabón Protex Clásico",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 8.00",
+        "bigImg": "protexBig",
+        "smallImg": "protexSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "13",
+        "name": "Crema Dental Colgate",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 9.00",
+        "bigImg": "dentalBig",
+        "smallImg": "dentalSmall",
+        "tipo": "cuidado"
+    },
+    {
+        "id": "14",
+        "name": "Enalapril 10mg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "enalaprilBig",
+        "smallImg": "enalaprilSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "15",
+        "name": "Colchicina 0.5mg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 13.00",
+        "bigImg": "colchicinaBig",
+        "smallImg": "colchicinaSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "16",
+        "name": "Paracetamol 500mg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 14.00",
+        "bigImg": "paracetamolBig",
+        "smallImg": "paracetamolSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "17",
+        "name": "Azithromycin 500mg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 15.00",
+        "bigImg": "azithromycinBig",
+        "smallImg": "azithromycinSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "18",
+        "name": "Clonazepam 0.5mg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 16.00",
+        "bigImg": "clonazepamBig",
+        "smallImg": "clonazepamSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "19",
+        "name": "Omeprazol 20mg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "omeprazolBig",
+        "smallImg": "omeprazolSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "20",
+        "name": "Enterogermina",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "enterogerminaBig",
+        "smallImg": "enterogerminaSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "21",
+        "name": "Vick VapoRub",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 10.00",
+        "bigImg": "vickBig",
+        "smallImg": "vickSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "22",
+        "name": "Abrilar Jarabe",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "abrilBig",
+        "smallImg": "abrilSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "23",
+        "name": "Panadol",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "panadolBig",
+        "smallImg": "panadolSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "24",
+        "name": "Doloral 400mg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 10.00",
+        "bigImg": "doloralBig",
+        "smallImg": "doloralSmall",
+        "tipo": "general"
+    },
+    {
+        "id": "25",
+        "name": "Aspirina",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "price": "S/. 12.00",
+        "bigImg": "aspirinaBig",
+        "smallImg": "aspirinaSmall",
+        "tipo": "general"
     }
 ]
